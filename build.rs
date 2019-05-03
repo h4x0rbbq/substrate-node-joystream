ef0d9b4c9484541f34821165f48b876eae3e2ee3
Matthew Roberts <matthew@roberts.pm>
2019-05-03 21:21:52 +1000

    Test speed hack that prob won't work.

--- build.rs
+++ build.rs
@@ -12,7 +12,7 @@ fn main() {
 	// Allow run time to be built in the background.
 	// Theoretically faster builds if dependencies in the node
 	// cargo are linked right.
-	while !Path::new("substrate-runtime-joystream").exists() {
+	while !Path::new("substrate-runtime-joystream/is_built").exists() {
 		thread::sleep(Duration::from_millis(1000));
 	}
 }
