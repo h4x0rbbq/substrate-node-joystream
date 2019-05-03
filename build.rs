84365c958ebdeb12e42292e303786d195922eab1
Matthew Roberts <matthew@roberts.pm>
2019-05-03 21:54:54 +1000

    try new exec.

--- build.rs
+++ build.rs
@@ -12,7 +12,8 @@ fn main() {
 	// Allow run time to be built in the background.
 	// Theoretically faster builds if dependencies in the node
 	// cargo are linked right.
-	while !Path::new("substrate-runtime-joystream/is_built").exists() {
-		thread::sleep(Duration::from_millis(1000));
+	while !Path::new("/home/travis/substrate-node-joystream/substrate-runtime-joystream/is_built").exists() {
+		thread::sleep(Duration::from_millis(10000));
+		println!("Waiting for runtime to be built...");
 	}
 }
