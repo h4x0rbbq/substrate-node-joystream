bf8614ad8bb511f2737af2b56d07a601f4f14531
Matthew Roberts <matthew@roberts.pm>
2019-05-03 21:12:34 +1000

    Test speed hack that prob won't work.

--- build.rs
+++ build.rs
@@ -1,8 +1,18 @@
 use vergen::{ConstantsFlags, generate_cargo_keys};
+use std::time::Duration;
+use std::thread;
+use std::path::Path;
 
 const ERROR_MSG: &'static str = "Failed to generate metadata files";
 
 fn main() {
 	generate_cargo_keys(ConstantsFlags::all()).expect(ERROR_MSG);
 	println!("cargo:rerun-if-changed=.git/HEAD");
+	
+	// Allow run time to be built in the background.
+	// Theoretically faster builds if dependencies in the node
+	// cargo are linked right.
+	while !Path::new("substrate-runtime-joystream").exists() {
+		thread::sleep(Duration::from_millis(1000));
+	}
 }
