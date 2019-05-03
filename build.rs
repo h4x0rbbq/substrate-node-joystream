129cc4715a94e38d6ed96e08092ee397ecc348c1
Matthew Roberts <matthew@roberts.pm>
2019-05-04 02:26:37 +1000

    build.rs optimization

--- build.rs
+++ build.rs
@@ -1,19 +1,8 @@
 use vergen::{ConstantsFlags, generate_cargo_keys};
-use std::time::Duration;
-use std::thread;
-use std::path::Path;
 
 const ERROR_MSG: &'static str = "Failed to generate metadata files";
 
 fn main() {
 	generate_cargo_keys(ConstantsFlags::all()).expect(ERROR_MSG);
 	println!("cargo:rerun-if-changed=.git/HEAD");
-	
-	// Allow run time to be built in the background.
-	// Theoretically faster builds if dependencies in the node
-	// cargo are linked right.
-	while !Path::new("/home/travis/substrate-node-joystream/substrate-runtime-joystream/is_built").exists() {
-		thread::sleep(Duration::from_millis(10000));
-		println!("Waiting for runtime to be built...");
-	}
 }
---
+++ speed_build.rs
@@ -0,0 +1,15 @@
+use std::time::Duration;
+use std::thread;
+use std::path::Path;
+
+const ERROR_MSG: &'static str = "Failed to generate metadata files";
+
+fn main() {
+	// Allow run time to be built in the background.
+	// Theoretically faster builds if dependencies in the node
+	// cargo are linked right.
+	while !Path::new("/home/travis/substrate-node-joystream/substrate-runtime-joystream/is_built").exists() {
+		thread::sleep(Duration::from_millis(10000));
+		println!("Waiting for runtime to be built...");
+	}
+}
